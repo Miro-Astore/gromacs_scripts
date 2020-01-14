fa10f4a0f03e2cc26106a87f502720f8b2d7a17a
Miro-Astore <miro.astore@gmail.com>
2020-01-14 12:22:55 +1100

    latest ionisation scripts

--- ionize_gro.sh
+++ ionize_gro.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 gmx grompp -f ions.mdp -c converted.gro -p topol.top -o ions.tpr -maxwarn 1
-gmx genion -s ions.tpr -p topol.top -o ionized.gro -neutral
+gmx genion -s ions.tpr -p topol.top -o ionized.gro -pname K -nname CL -neutral
