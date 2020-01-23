9497ee396d1dff3c263c8ab2214a68c10300aad5
Miro-Astore <miro.astore@gmail.com>
2020-01-23 14:09:38 +1100

    fixed behaviour, navigating around lipids, need a better long term solution

--- make_psf_from_itps.sh
+++ make_psf_from_itps.sh
@@ -11,7 +11,7 @@ mkdir -p psf_buildfiles
 
 list_itp=$(cat $top_file  | awk '/\[\s*molecules\s*\]/{y=1;next}y' | grep -v "^;" | grep -v "^\#" | grep -v "SOL" | awk '{print $1}')
 	# exclude lipids because they are not well behaved
-list_itp=$(echo $list_itp | grep -v "Other_chain_L")
+echo $list_itp
 
 echo "package require psfgen" > psf_buildfiles/merge_elements.tcl
 
@@ -19,6 +19,10 @@ echo "package require psfgen" > psf_buildfiles/merge_elements.tcl
 #convert sanely 
 for i in $( echo $list_itp);
 do 
+	if [ $i = "Other_chain_L" ]
+	then
+		echo "readpsf psf_buildfiles/lipid.psf" >> psf_buildfiles/merge_elements.tcl
+	else
 	#using ,'s as delims because ^ is a part of the regex pattern we're using here
 	#using the greedy nature of bash regex properly. Usually we would have to go and use perl or something and ew who wants to do that.
 	segname=$( echo $i | sed "s,^.*_,,g")
@@ -28,14 +32,14 @@ do
 	#give things a unique segname so we can differentiate them when we merge all the psf files back together. It gets mad otherwise. It would be nice to give them real segnames but vmd doesn't like trying to write psf file without a segname and that is going to be a whole other kettle of fish to try and deal with to be honest.  
 	sed -i "s/MAIN/$segname   /g" psf_buildfiles/$i.psf
 	echo "readpsf psf_buildfiles/$i.psf" >> psf_buildfiles/merge_elements.tcl
+fi
 done
 
 # make a psf of the water in the system
-cat write_water.tcl | sed -e "s^PSFFILE^$psf_file^g" | sed -e "s^PDBFILE^$pdb_file^g" > psf_buildfiles/write_water.tcl
-vmd -dispdev text -e  psf_buildfiles/write_water.tcl
+cat write_water_lipids.tcl | sed -e "s^PSFFILE^$psf_file^g" | sed -e "s^PDBFILE^$pdb_file^g" > psf_buildfiles/write_water_lipids.tcl
+vmd -dispdev text -e  psf_buildfiles/write_water_lipids.tcl
 
 echo "readpsf psf_buildfiles/waterpdb.psf" >> psf_buildfiles/merge_elements.tcl
-echo "readpsf psf_buildfiles/lipid.psf" >> psf_buildfiles/merge_elements.tcl
 echo "writepsf ../ionized.psf" >> psf_buildfiles/merge_elements.tcl
 echo "exit" >> psf_buildfiles/merge_elements.tcl
 vmd -dispdev text -e psf_buildfiles/merge_elements.tcl 
