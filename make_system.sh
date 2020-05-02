083717069cbd9e90a188402c7a4bc6d26bf26efd
Miro-Astore <miro.astore@gmail.com>
2020-05-03 01:14:22 +1000

    cleaned up directory and created splits for artemis and gadi and glob and memb type simulations

--- make_system.sh
+++ make_system.sh
@@ -1,14 +1,10 @@
-rm memb.gro
-rm topol.top
-rm box.gro
-rm ionized.gro
-rm solv.gro 
-rm solvate.gro
-rm ions.tpr
 
-gmx pdb2gmx -f l.pdb -o memb.gro -ff charmm36-nov2016-newvs -water tip3 -vsite hydrogens
-gmx editconf -f memb.gro -o box.gro -c yes -box 10.0 10.0 9.0  
-gmx solvate -cp box.gro -cs spc216 -o water.gro  -radius 0.7 -p topol.top
-gmx editconf -f water.gro -o solvate.gro -c yes -box 11.0 11.0 9.0  
+#gmx pdb2gmx -f l.pdb -o memb.gro -ff charmm36-nov2016-newvs -water tip3 -vsite hydrogens
+gmx pdb2gmx -f cut_autopsf.pdb -o cut.gro -ff charmm36-mar2019 -water tip3 -ter
+gmx editconf -f cut.gro -o box.gro -c yes -d 1.2
+gmx solvate -cp box.gro -cs spc216 -o solvate.gro  -radius 0.7 -p topol.top
 
-bash ./gromacs_scripts/ionize_gro.sh
+gmx grompp -f gromacs_scripts/ions.mdp -c solvate.gro -p topol.top -o ions.tpr -maxwarn 1
+gmx genion -s ions.tpr -p topol.top -o ionized.gro -pname NA -nname CL -neutral -conc 0.15
+
+echo -e "q\n" | gmx make_ndx  -f ionized.gro -o index.ndx
